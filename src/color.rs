1817cd122d98bef2638253ec9a28cbfe28fec855
mango0110 <1119369173@qq.com>
2018-10-06 13:00:28 +0800

    color display

--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,6 @@
+use std::fmt::Display;
+use std::fmt::Error;
+use std::fmt::Formatter;
 use super::vector3::*;
 
 pub struct Color(Vec3);
@@ -10,4 +13,10 @@ impl Color {
     pub fn r(&self) -> f32 { self.0.x() }
     pub fn g(&self) -> f32 { self.0.y() }
     pub fn b(&self) -> f32 { self.0.z() }
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        self.0.fmt(f)
+    }
 }
