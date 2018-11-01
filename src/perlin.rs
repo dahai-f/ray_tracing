a22de4b7e241e5f04d8705b5c232055dc3b81c58
mango0110 <1119369173@qq.com>
2018-11-01 22:50:27 +0800

    perlin noise

--- src/perlin.rs
+++ src/perlin.rs
@@ -1,11 +1,45 @@
 use crate::*;
+use rand::prelude::*;
+
 
 pub struct Perlin {
-
+    ran_float: [f32; 256],
+    perm_x: [i32; 256],
+    perm_y: [i32; 256],
+    perm_z: [i32; 256],
 }
 
+
 impl Perlin {
-    pub fn a(){
-        let a = vec![1; 5];
+    const fn new() -> Perlin {
+        Perlin {
+            ran_float: perlin_generate(),
+            perm_x: perlin_generate_perm(),
+            perm_y: perlin_generate_perm(),
+            perm_z: perlin_generate_perm(),
+        }
     }
+}
+
+const fn perlin_generate() -> [f32; 256] {
+    Random::with_rng(|rng| {
+        let mut ran_float = [0.0; 256];
+        let a = ran_float;
+        let b = ran_float;
+        for i in 0..256 {
+            ran_float[i] = rng.gen();
+        }
+        ran_float
+    })
+}
+
+const fn permute(p: &mut [i32; 256]) {}
+
+const fn perlin_generate_perm() -> [i32; 256] {
+    let mut p = [0_i32; 256];
+    for i in 0_i32..256 {
+        p[i as usize] = i;
+    }
+    permute(&mut p);
+    p
 }
